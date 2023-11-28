ad223dbed7be2bc4c77dd59629f6553f47bf3df8
Cyril De Faria <cdefaria@qosenergy.com>
2023-11-28 09:23:45 +0100

    [FIX] Avoid one letter vars.

--- clerk/organizations.py
+++ clerk/organizations.py
@@ -10,22 +10,22 @@ class OrganizationsService(Service):
     async def list(self) -> List[types.Organization]:
         """Retrieve a list of all organizations"""
         async with self._client.get(self.endpoint) as resp:
-            resp_json = await r.json()
-            return [types.Organization.parse_obj(obj) for obj in s.get("data", [])]
+            resp_json = await resp.json()
+            return [types.Organization.parse_obj(obj) for obj in resp_json.get("data", [])]
 
     async def get(self, organization_id: str) -> types.Organization:
         """Retrieve an organization by their id"""
-        async with self._client.get(f"{self.endpoint}/{organization_id}") as r:
-            return types.Organization.parse_obj(await r.json())
+        async with self._client.get(f"{self.endpoint}/{organization_id}") as resp:
+            return types.Organization.parse_obj(await resp.json())
 
     async def delete(self, organization_id: str) -> types.DeleteOrganizationResponse:
         """Delete an organization by their id"""
-        async with self._client.delete(f"{self.endpoint}/{organization_id}") as r:
-            return types.DeleteOrganizationResponse.parse_obj(await r.json())
+        async with self._client.delete(f"{self.endpoint}/{organization_id}") as resp:
+            return types.DeleteOrganizationResponse.parse_obj(await resp.json())
 
     async def update(self, organization_id: str, request: types.UpdateOrganizationRequest) -> types.Organization:
         """Update an organization by their id"""
         async with self._client.patch(
             f"{self.endpoint}/{organization_id}", data=request.json(exclude_unset=True)
-        ) as r:
-            return types.Organization.parse_obj(await r.json())
+        ) as resp:
+            return types.Organization.parse_obj(await resp.json())
